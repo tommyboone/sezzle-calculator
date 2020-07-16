650cf421c09459700dfbb1853c2fc73ffd5cba16
Tommy Boone <tommyboone23@Tommys-MacBook-Air.local>
2020-07-16 11:50:18 -0500

    updated build folder

---
+++ client/build/precache-manifest.6e01bddc0981dd057b6833d74bead470.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "054eb015d0f633408bcf887836b9887d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d4e9ec7bdbbc29c49c5",
+    "url": "/static/css/main.a4f82969.chunk.css"
+  },
+  {
+    "revision": "42170fc6eab572c38f9b",
+    "url": "/static/js/2.5e22a736.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5e22a736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d4e9ec7bdbbc29c49c5",
+    "url": "/static/js/main.84bcd7f5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
